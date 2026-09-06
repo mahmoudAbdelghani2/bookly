7d01784b018bc0fe9830d129014c88fc649e817b
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-09-06 21:48:12 +0300

    refactor &  navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_screen.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 
 class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
@@ -17,6 +21,20 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
+
+    navigateToHomeScreen();
+  }
+
+  void navigateToHomeScreen() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeScreen(),
+          transition: Transition.fade,
+          duration: const Duration(milliseconds: 250));
+    });
+  }
+
+  void initSlidingAnimation() {
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
