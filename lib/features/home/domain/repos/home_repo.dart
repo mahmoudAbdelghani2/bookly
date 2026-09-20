a076f35cf8112b423d61836f0d8956e16612e2cc
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-09-20 23:41:26 +0300

    add dartz

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/errors/failuer.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failuer, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failuer, List<BookEntity>>> fetchNewestBooks();
 }
