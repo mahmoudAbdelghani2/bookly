b662ceb332a3ecb01a9ccf6b7da175e5ecd80e16
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-09-20 22:45:32 +0300

    domain layer for home feature

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num rating;
+  final num price;
+
+  BookEntity({
+    required this.image,
+    required this.title,
+    required this.authorName,
+    required this.rating,
+    required this.price,
+  });
+}
