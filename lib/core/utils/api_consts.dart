a4d48063642117d003ce268a8e5395ad240b54dc
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-09-21 17:15:05 +0300

    small update on dio services

---
+++ lib/core/utils/api_consts.dart
@@ -0,0 +1,3 @@
+class ApiConsts {
+  static const String baseUrl = 'https://www.googleapis.com/books/v1/';
+}
--- lib/core/utils/dio_services.dart
+++ lib/core/utils/dio_services.dart
@@ -1,17 +1,41 @@
+import 'package:bookly/core/utils/api_consts.dart';
 import 'package:bookly/core/utils/api_consumer.dart';
 import 'package:dio/dio.dart';
 
 class DioServices extends ApiConsumer {
-  final Dio _dio;
-  final baseUrl = 'https://www.googleapis.com/books/v1/';
-  DioServices(this._dio);
+  final Dio dio;
+  DioServices(this.dio) {
+    _initDio();
+  }
+
+  void _initDio() {
+    dio.options.baseUrl = ApiConsts.baseUrl;
+    dio.options.connectTimeout = const Duration(seconds: 15);
+    dio.options.receiveTimeout = const Duration(seconds: 15);
+
+    // dio.interceptors.add(
+    //   InterceptorsWrapper(
+    //     onRequest: (options, handler) async {
+    //       String? token = await CacheHelper.getToken();
+    //       if (token != null && token.isNotEmpty) {
+    //         options.headers['Authorization'] = 'Bearer $token';
+    //       }
+    //       handler.next(options);
+    //     },
+    //     onError: (DioException e, handler) {
+    //       handler.next(e);
+    //     },
+    //   ),
+    // );
+  }
+
   @override
   Future<Map<String, dynamic>> get(
       {required String endpoint,
       Map<String, dynamic>? queryParameters,
       Map<String, dynamic>? body,
       Map<String, dynamic>? headers}) async {
-    Response response = await _dio.get('$baseUrl$endpoint');
+    Response response = await dio.get('${ApiConsts.baseUrl}$endpoint');
     return response.data;
   }
 }
