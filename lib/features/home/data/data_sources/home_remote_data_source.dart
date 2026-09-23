6f9d856341cb7d72916e6bb0aa5d14cfda01e738
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-09-23 18:31:11 +0300

    implement fetch featured books

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,11 +1,27 @@
+import 'package:bookly/core/utils/api_consumer.dart';
 import 'package:bookly/features/home/data/data_sources/contract.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
+  final ApiConsumer apiConsumer;
+  HomeRemoteDataSourceImpl(this.apiConsumer);
+
+  List<BookEntity> getBooksList(Map<String, dynamic> response) {
+    List<BookEntity> books = [];
+    for (var book in response['items']) {
+      books.add(BookModel.fromJson(book));
+    }
+    return books;
+  }
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var response = await apiConsumer.get(
+      endpoint: 'volumes?Filtering=free-ebooks&q=Programming',
+    );
+    List<BookEntity> books = getBooksList(response);
+    return books;
   }
 
   @override
